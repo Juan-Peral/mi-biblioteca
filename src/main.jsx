0bf73a041d70c30becca517b5e1c2ee661cc75f5
Juan-Peral <j.peral.ia2026@gmail.com>
2026-05-13 14:52:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react"; 
 import ReactDOM from "react-dom/client";
 
 // ── SUPABASE ───────────────────────────────────────────────────────────────────
